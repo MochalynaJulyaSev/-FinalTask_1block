d4ad7622029f5c4ed645232cafbb8d4dee733e52
MochalynaJulyaSev <133509444+MochalynaJulyaSev@users.noreply.github.com>
2023-11-07 19:48:01 +0300

    Сортировка элементов

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+string[] basicArray =  {"sheep", "hi", "world", "cat", "dog", "hamster"};
+
+int GetSizeNewArray(string[] array) 
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
